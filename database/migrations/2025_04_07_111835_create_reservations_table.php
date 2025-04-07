df1550fc2dd919bde883f36a5055d5bd14b2d6cb
Hichamelhmaydi <ct1594065@gmail.com>
2025-04-07 17:17:52 +0100

    creation le migration de reservation

--- database/migrations/2025_04_07_111835_create_reservations_table.php
+++ database/migrations/2025_04_07_111835_create_reservations_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('voyage_id')->constrained('voyages');
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('statut',['en attente','en cours','accepté','refusé'])->default('en attente');
+            $table->dateTime('date_reservation');
+            $table->float('prix_total');
+            $table->string('notes');
             $table->timestamps();
         });
     }
