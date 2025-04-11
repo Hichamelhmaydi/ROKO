b61e14670975e5b13c5bd463c5995ac9cefae5f4
Hichamelhmaydi <ct1594065@gmail.com>
2025-04-11 15:36:26 +0100

    ajouter la foction pour ajoueter un Personel

--- app/Models/Personel.php
+++ app/Models/Personel.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Personel extends Authenticatable
-{
+
+class Personel extends Authenticatable implements JWTSubject{
+    use HasFactory;
     protected $table = 'personels'; 
     protected $fillable = ['nom', 'prenom', 'email', 'telephone', 'adrees', 'N°_cart', 'N°_passport', 'image_chemain'];
     protected $hidden = [
--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,4 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']); 
 Route::post('logout', [AuthController::class, 'logout']);
 Route::get('me', [AuthController::class, 'me']);
+Route::post('personel', [AuthController::class, 'AjouterPersonel']);
